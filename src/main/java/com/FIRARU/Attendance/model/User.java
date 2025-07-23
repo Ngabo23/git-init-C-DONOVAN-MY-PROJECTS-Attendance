b2fc02aca12cae244c552b51e4ea206e5b24fdd2
Ngabonziza Fiston <144804487+Ngabo23@users.noreply.github.com>
2025-07-23 14:03:24 +0200

    Create User.java

---
+++ src/main/java/com/FIRARU/Attendance/model/User.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.FIRARU.Attendance.model;
+
+/**
+ *
+ * @author Donovan
+ */
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role; // "ADMIN" or "CLERK"
+}
